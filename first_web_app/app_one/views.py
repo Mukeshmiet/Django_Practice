1770cfa07b873565d123b9e8f8dbd886d7c77f08
Mukesh Kumar <mukesh.kumar.me.2018@miet.ac.in>
2023-08-09 18:06:43 +0530

    html_template

--- first_web_app/app_one/views.py
+++ first_web_app/app_one/views.py
@@ -4,4 +4,5 @@ from django.urls import reverse
 # Create your views here.
 
 def simple_view(request):
-    return render(request, 'app_one/index.htm')
+    dict_name = {"name": "User_name"}
+    return render(request, 'app_one/index.htm', context=dict_name)
