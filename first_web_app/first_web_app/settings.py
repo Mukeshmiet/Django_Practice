829d434aa2d34de8f42784900bc5e1ac34da6f9f
Mukesh Kumar <mukesh.kumar.me.2018@miet.ac.in>
2023-08-17 23:38:25 +0530

    Model and Templates

--- first_web_app/first_web_app/settings.py
+++ first_web_app/first_web_app/settings.py
@@ -59,7 +59,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
-        # 'DIRS': [os.path.join(BASE_DIR, 'templates/')],
+        # 'DIRS': [os.path.join(BASE_DIR, 'templates/')],           # for templates in root folder
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
