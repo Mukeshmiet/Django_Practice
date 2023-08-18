2bce6b1375c86c8b301dfe512674df27b2261c08
Mukesh Kumar <mukesh.kumar.me.2018@miet.ac.in>
2023-08-18 23:07:40 +0530

    Data from DB to Templates

--- project_dental/app_office/views.py
+++ project_dental/app_office/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from . models import Patient
+
 
 # Create your views here.
+def patients(request):
+    all_patients = Patient.objects.all()
+    context = {'all_patients': all_patients}   # list into dict object
+    return render(request, 'app_office/patients.html', context = context)
