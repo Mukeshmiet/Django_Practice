35c7f3c7328383f142513c49b369bafbddb3a02a
Mukesh Kumar <mukesh.kumar.me.2018@miet.ac.in>
2023-08-18 20:06:39 +0530

    Using SQL

--- project_dental/project_dental/wsgi.py
+++ project_dental/project_dental/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for first_web_app project.
+WSGI config for project_dental project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'first_web_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_dental.settings')
 
 application = get_wsgi_application()
