efa2ae3d6e06ecf1f4233a72d675deb1beea589d
Mukesh K <76239820+Mukeshmiet@users.noreply.github.com>
2024-02-14 12:00:22 +0530

    new project

--- README.md
+++
@@ -1,2 +0,0 @@
-# Django_Practice
-I created this repo. for practicing Django concepts along with some projects. Its contains mai practices codes and data I have used during my practicing.
--- blackcoffee/dash/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import get_dashboard_data, index
-
-urlpatterns = [
-    path('api/dashboard_data/', get_dashboard_data, name='get_dashboard_data'),
-    path('', index, name='index'),
-]
